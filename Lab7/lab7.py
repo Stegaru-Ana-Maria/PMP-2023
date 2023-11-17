2e110edd89439fc5312deef4b1f2f633cc6cc224
Stegaru-Ana-Maria <100145356+Stegaru-Ana-Maria@users.noreply.github.com>
2023-11-17 21:50:10 +0200

    Update lab7.py

--- Lab7/lab7.py
+++ Lab7/lab7.py
@@ -1,12 +1,34 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pymc as pm
+import pymc
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+import arviz as az
 
-df = pd.read_csv('auto-mpg.csv')
+# Punctul a
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+link = 'https://drive.google.com/file/d/1QFTznskcFmqNUXtx5fuMlisH3rSKG_Zj/view'
+id = link.split('/')[-2]
 
+downloaded = drive.CreateFile({'id': id})
+downloaded.GetContentFile('auto-mpg.csv')
+data = pd.read_csv('auto-mpg.csv')
 
-df = df.dropna()
+to_drop = ['cylinders', 'displacement', 'weight', 'acceleration', 'model year', 'origin', 'car name']
+df = data.drop(to_drop, axis=1)
+df.replace('?', np.nan, inplace=True)
+df= df.dropna()
+df = df.astype(float)
+df= df[df['mpg'] > 0]
+
+X = df['horsepower'].values
+y = df['mpg'].values
 
 plt.figure(figsize=(10, 6))
 plt.scatter(df['horsepower'], df['mpg'], color='blue', alpha=0.5)
@@ -16,25 +38,34 @@ plt.ylabel('Mile pe Galon (mpg)')
 plt.grid(True)
 plt.show()
 
-X = df['horsepower'].values
-y = df['mpg'].values
+# Punctul b
+with pymc.Model() as model:
+    alpha = pymc.Normal('alpha', mu=0, tau=0.001)
+    beta = pymc.Normal('beta', mu=0, tau=0.001)
+    epsilon = pymc.HalfCauchy('epsilon', 5)
+    x = pymc.Normal('x', mu=np.mean(X), tau=1.0/np.var(X), observed=X)
+    mu = alpha + beta * x
+    y = pymc.Normal('y', mu=mu, sigma=epsilon,observed=df['mpg'])
 
+    trace = pymc.sample(10000, tune=5000, return_inferencedata=True)
 
-with pm.Model() as linear_model:
-    alpha = pm.Normal('alpha', mu=0, tau=1.0 / 10**2)
-    beta = pm.Normal('beta', mu=0, tau=1.0 / 10**2)
-    sigma = pm.Uniform('sigma', lower=0, upper=10)
+# Punctul c
+plt.plot(df['horsepower'], df['mpg'], 'C0.')
+posterior_g = trace.posterior.stack(samples={"chain", "draw"})
+alpha_m = posterior_g['alpha'].mean().item()
+beta_m = posterior_g['beta'].mean().item()
+draws = range(0, posterior_g.samples.size, 10)
+plt.plot(df['horsepower'], posterior_g['alpha'][draws].values + posterior_g['beta'][draws].values * df['horsepower'][:, None], c='gray', alpha=0.5)
+plt.plot(df['horsepower'], alpha_m + beta_m * df['horsepower'], c='k', label=f'y = {alpha_m:.2f} + {beta_m:.2f} * x')
+plt.xlabel('x')
+plt.ylabel('y', rotation=0)
+plt.legend()
 
-    mu = alpha + beta * df['CP'].values
-    likelihood = pm.Normal('mpg', mu=mu, tau=1.0 / sigma**2, observed=df['mpg'].values)
-
-
-with linear_model:
-    trace = pm.sample(2000, tune=1000)
-
-
-
-alpha_mean = np.mean(trace['alpha'])
-beta_mean = np.mean(trace['beta'])
-
-regression_lines = alpha_samples + beta_samples[:, None] * df['CP'].values
+# Punctul d
+az.plot_posterior(trace, hdi_prob=0.95, var_names=['alpha', 'beta'])
+plt.scatter(df['horsepower'], df['mpg'], color='blue', alpha=0.5, label='Observations')
+plt.title('Relația dintre Cai Putere și Consumul de Combustibil (mpg) cu Regiunea 95%HDI')
+plt.xlabel('Cai Putere (CP)')
+plt.ylabel('Mile pe Galon (mpg)')
+plt.legend()
+plt.show()
